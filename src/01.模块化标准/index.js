bb774b8f291039f212229ff28926f5cd0825946b
lcl <2251086272@qq.com>
2025-06-19 17:01:13 +0800

    6-19

---
+++ src/01.模块化标准/index.js
@@ -0,0 +1,7 @@
+const data = require("./es6");
+
+console.log("data:", data, data.a);
+
+import * as data1 from "./common";
+
+console.log("data1:", data1);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-/**
- * @Author: lcl
- * @Date: 2025/6/18
- */
-import _ from "lodash";
-function component() {
-  const element = document.createElement("div");
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
-}
-
-document.body.appendChild(component());
+import "./01.模块化标准";
