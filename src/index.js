e02b703035d7ed7aa7f56ebb802197f723bc948a
lcl <2251086272@qq.com>
2025-07-11 10:34:06 +0800

    publicPath 学习

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import "./02.扩展生态介绍/05.处理图片的loader";
+/**
+ * @Author: lcl
+ * @Date: 2025/6/25
+ */
+import "./02.扩展生态介绍/06.punlicPath";
+console.log("__webpack_public_path__", __webpack_public_path__);
