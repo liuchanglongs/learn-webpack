a949bcc4f5d095c0df98e63438bc1fe3678a2f30
lcl <2251086272@qq.com>
2025-07-17 19:47:59 +0800

    减少模块解析

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@
  * @Author: lcl
  * @Date: 2025/6/25
  */
-import "./03.css的工程化学习/03.生成独立的css静态资源";
-// console.log("__webpack_public_path__", __webpack_public_path__);
+import "./05.性能优化/01.减少模块解析";
